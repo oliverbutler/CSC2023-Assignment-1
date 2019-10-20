bc106e5586b9c7f35660184264956879dd598d77
Oliver Butler <olly@protoncloud.co.uk>
2019-10-20 22:37:21 +0100

    Implemented printing

--- src/app/TestSort.java
+++ src/app/TestSort.java
@@ -6,28 +6,23 @@
 /*************************************************/
 
 
-public class TestSort
-{
-	public static void main(String[] args) 
-    {
-        Sort sortTest = new Sort(50);
-        
-        /** Some simple tests using class Sort **/
-        
-        /** Display two comparison counters **/
-        System.out.println("\n\nInsertion sort comparison counter: " + sortTest.compIS);
-        System.out.println("Quicksort comparison counter: " + sortTest.compQS);
-        
-        /** Read in test data into array **/
-        sortTest.readIn("src/app/test1.txt");
-        
-        /** Display array **/
-        sortTest.display(15,"Values in test1.txt");
+public class TestSort {
+	public static void main(String[] args) {
 
-        sortTest.insertion();
-        sortTest.display(15, "Insertion sorted");
+    int testResults[][] = new int[3][6];
 
-        
+    for(int alg=0; alg<3; alg++) {
+      for(int test=0; test<6; test++) {
+        Sort sortTest;
+        if(test<3) sortTest = new Sort(50);
+        else sortTest = new Sort(1000);
+        sortTest.readIn("src/app/test" + (test+1) + ".txt");
+        sortTest.sort(alg);
+        testResults[alg][test] = sortTest.comp;
+      }
     }
-    
+    System.out.format("%10s |%10s |%10s |%10s\n", "Test", "Insertion", "Quicksort", "Newsort");
+    for(int i=0; i<6; i++)
+      System.out.format("%10s |%10d |%10d |%10d\n", "test" + (i+1) + ".txt", testResults[0][i], testResults[1][i], testResults[2][i]);
+  } 
 } /** End of TestSort class **/
